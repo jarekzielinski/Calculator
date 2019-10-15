0100cf0a8253e7767fc4769a05f27ad91d412e92
Jarosław Zieliński <jaroslawzielinski1986@wp.pl>
2019-10-15 17:30:10 +0200

    Added onClick method and keystyle to buttons

--- app/src/main/java/pl/java/calculator/MainActivity.java
+++ app/src/main/java/pl/java/calculator/MainActivity.java
@@ -2,6 +2,9 @@ package pl.java.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void keyClicked(View view) {
+        Button button=(Button) view;
+        String key=button.getText().toString();
+        TextView textview=(TextView) findViewById(R.id.textView);
+        textview.setText(textview.getText().toString()+key);
     }
 }
